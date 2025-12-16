ddf5a1e071efbe6e106e43b1e7a1c4599e90bb9c
Imtanan Ahnaf <bc200401263@vu.edu.pk>
2025-12-16 22:42:19 +0500

    utils code discussion

--- src/index.js
+++ src/index.js
@@ -1,11 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
 dotenv.config({
   path: "./env",
 });
+app.on("error", (error) => {
+  console.log("Error: ", error);
+  throw error;
+});
 
-connectDB();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(` Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongo db connection failed !!! ", error);
+  });
 
 /*
 const app = express();
