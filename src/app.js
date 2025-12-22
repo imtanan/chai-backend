fcdd847404462a6f8ce058316b9426784a72a43d
Imtanan Ahnaf <bc200401263@vu.edu.pk>
2025-12-22 18:29:30 +0500

    all setup configured

--- src/app.js
+++ src/app.js
@@ -14,4 +14,43 @@ app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
+//import routes
+import userRouter from "./routes/user.routes.js";
+app.use("/api/v1/users", userRouter);
 export { app };
+
+/*
+Information:
+express.use()
+
+👉 Means: “Use this helper for every request.”
+
+🧠 Why world uses it?
+
+So we don’t repeat code
+
+Same rule applies to all routes
+
+Example:
+
+Every request → pass through security gate
+*/
+
+/*
+cors
+
+👉 Means: “Allow frontend to talk to backend.”
+
+🧠 Why world uses it?
+
+Browser blocks requests by default
+
+Frontend (React) ≠ Backend (API)
+
+Without CORS → ❌ request blocked
+
+Think:
+
+Frontend knocking door
+CORS says: “Yes, you are allowed”
+*/
